90f9e612d8c8781c8f86bba57f2dcd533875f7ca
Nick <91141907+seoulxss@users.noreply.github.com>
2024-07-04 19:01:35 +0200

    Update ConfigLib.h

--- ConfigLib.h
+++ ConfigLib.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "src/Config.h"
-
 /*MIT License
 
 Copyright (c) 2024 seoulxss
@@ -23,3 +21,4 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+#include "src/Config.h"
